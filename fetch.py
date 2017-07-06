2b54655abf941009b27b90a901a5af03f10489b3
Aporter53 <pavelow53.andrew@gmail.com>
2017-07-05 22:11:19 -0400

    Correctly parsing CSV using csv module

--- fetch.py
+++ fetch.py
@@ -2,6 +2,7 @@ import datetime as dt
 import requests
 import re
 import time
+import csv
 # search with regular expressions
 
 # "CrumbStore":\{"crumb":"(?<crumb>[^"]+)"\}
@@ -15,7 +16,7 @@ txt = r.text # extract html
 cookie = r.cookies['B'] # the cooke we're looking for is named 'B'
 print('Cookie: ', cookie)
 
-# Now we need to extract the token from html. 
+# Now we need to extract the token from html.
 # the string we need looks like this: "CrumbStore":{"crumb":"lQHxbbYOBCq"}
 # regular expressions will do the trick!
 
@@ -25,19 +26,22 @@ for line in txt.splitlines():
     m = pattern.match(line)
     if m is not None:
         crumb = m.groupdict()['crumb']
-        
-        
+
+
 print('Crumb=',crumb)
-start = dt.datetime(2007,1,1).timestamp()
-end = dt.datetime(2007,1,1).timestamp()
+start = str(int(dt.datetime(2007,1,1).timestamp()))
+end = str(int(time.time()))
+ticker = "GEVO"
+ticker = ticker.upper()
+
 #or int(time.time()) for current timestamp
-url = "https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=1467662202&period2=1499198202&interval=1d&events=history&crumb=" + crumb
+url = "https://query1.finance.yahoo.com/v7/finance/download/" + ticker + "?period1=" + start + "&period2=" + end + "&interval=1d&events=history&crumb=" + crumb
 data = requests.get(url, cookies={'B':cookie})
-#data = data.text
+content = data.content.decode("utf-8")
+csv_content = csv.reader(content.splitlines(), delimiter=',')
 
-for line in data:
-    print(line.decode("utf-8"))
-    print()
 
-#use this for printing csv
-#https://stackoverflow.com/questions/35371043/use-python-requests-to-download-csv
+#['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
+for row in list(csv_content):
+    if len(row) == 7:  # only get correctly formatted rows
+        print(row)
