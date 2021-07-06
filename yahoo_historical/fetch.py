31d1d50f8643e8f2d042f0c30b7ee564154c176b
AndrewRPorter <pavelow53.andrew@gmail.com>
2021-07-05 20:16:08 -0400

    Remove cookie and crumb due to yahoo finance api changes

--- yahoo_historical/fetch.py
+++ yahoo_historical/fetch.py
@@ -1,6 +1,5 @@
 import calendar as cal
 import datetime as dt
-import re
 import time
 import warnings
 
@@ -14,15 +13,12 @@ except ImportError:
 
 
 class Fetcher:
-    api_url = (
-        "https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%s&period2=%s&interval=%s&events=%s&crumb=%s"
-    )
+    api_url = "https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%s&period2=%s&interval=%s&events=%s"
 
     def __init__(self, ticker, start, end=None, interval="1d"):
         """Initializes class variables and formats api_url string"""
         self.ticker = ticker.upper()
         self.interval = interval
-        self.cookie, self.crumb = self.init()
         self.start = int(cal.timegm(dt.datetime(*start).timetuple()))
 
         if end is not None:
@@ -30,59 +26,60 @@ class Fetcher:
         else:
             self.end = int(time.time())
 
-    def init(self):
-        """Returns a tuple pair of cookie and crumb used in the request"""
-        url = "https://finance.yahoo.com/quote/%s/history" % (self.ticker)
-        r = requests.get(url)
-        txt = r.content
-        cookie = r.cookies["B"]
-        pattern = re.compile('.*"CrumbStore":\{"crumb":"(?P<crumb>[^"]+)"\}')
-
-        for line in txt.splitlines():
-            m = pattern.match(line.decode("utf-8"))
-            if m is not None:
-                crumb = m.groupdict()["crumb"]
-                crumb = crumb.replace(u"\\u002F", "/")
-        return cookie, crumb  # return a tuple of crumb and cookie
-
     def _get(self, events):
         if self.interval not in ["1d", "1wk", "1mo"]:
             raise ValueError("Incorrect interval: valid intervals are 1d, 1wk, 1mo")
 
-        url = self.api_url % (self.ticker, self.start, self.end, self.interval, events, self.crumb)
+        url = self.api_url % (self.ticker, self.start, self.end, self.interval, events)
 
-        data = requests.get(url, cookies={"B": self.cookie})
+        data = requests.get(url, cookies={"User-agent": "Mozilla/5.0"})
         content = StringIO(data.content.decode("utf-8"))
         return pd.read_csv(content, sep=",")
 
     def getData(self, events):
         """Returns a list of historical data from Yahoo Finance"""
-        warnings.warn("getData has been deprecated, use get_data instead", DeprecationWarning)
+        warnings.warn(
+            "getData has been deprecated, use get_data instead", DeprecationWarning
+        )
         return self._get(events)
 
     def getHistorical(self):
         """Returns a list of historical price data from Yahoo Finance"""
-        warnings.warn("getHistorical has been deprecated, use get_historical instead", DeprecationWarning)
+        warnings.warn(
+            "getHistorical has been deprecated, use get_historical instead",
+            DeprecationWarning,
+        )
         return self._get("history")
 
     def getDividends(self):
         """Returns a list of historical dividends data from Yahoo Finance"""
-        warnings.warn("getDividends has been deprecated, use get_dividends instead", DeprecationWarning)
+        warnings.warn(
+            "getDividends has been deprecated, use get_dividends instead",
+            DeprecationWarning,
+        )
         return self._get("div")
 
     def getSplits(self):
         """Returns a list of historical splits data from Yahoo Finance"""
-        warnings.warn("getSplits has been deprecated, use get_splits instead", DeprecationWarning)
+        warnings.warn(
+            "getSplits has been deprecated, use get_splits instead", DeprecationWarning
+        )
         return self._get("split")
 
     def getDatePrice(self):
         """Returns a DataFrame for Date and Price from getHistorical()"""
-        warnings.warn("getDatePrice has been deprecated, use get_date_price instead", DeprecationWarning)
+        warnings.warn(
+            "getDatePrice has been deprecated, use get_date_price instead",
+            DeprecationWarning,
+        )
         return self.getHistorical().iloc[:, [0, 4]]
 
     def getDateVolume(self):
         """Returns a DataFrame for Date and Volume from getHistorical()"""
-        warnings.warn("getDateVolume has been deprecated, use get_date_volume instead", DeprecationWarning)
+        warnings.warn(
+            "getDateVolume has been deprecated, use get_date_volume instead",
+            DeprecationWarning,
+        )
         return self.getHistorical().iloc[:, [0, 6]]
 
     def get_historical(self):
